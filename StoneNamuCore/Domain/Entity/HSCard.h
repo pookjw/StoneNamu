6dbd2444a69891347458c37522a83f89e889a364
Jinwoo Kim <kidjinwoo@me.com>
2021-07-18 05:11:33 +0900

    Removed retain attribute (because this is default)

--- StoneNamuCore/Domain/Entity/HSCard.h
+++ StoneNamuCore/Domain/Entity/HSCard.h
@@ -14,22 +14,22 @@ NS_ASSUME_NONNULL_BEGIN
 @interface HSCard : NSObject
 @property (readonly) NSInteger id;
 @property (readonly) BOOL collectible;
-@property (readonly, retain) NSString *slug;
+@property (readonly) NSString *slug;
 @property (readonly) HSCardClass classId;
-@property (readonly, retain) NSArray<NSNumber *> *multiClassIds;
+@property (readonly) NSArray<NSNumber *> *multiClassIds;
 @property (readonly) NSInteger cardSetId;
 @property (readonly) HSCardRarity rarityId;
-@property (readonly, retain) NSString *artistName;
+@property (readonly) NSString *artistName;
 @property (readonly) NSInteger health;
 @property (readonly) NSInteger attack;
 @property (readonly) NSInteger manaCost;
-@property (readonly, retain) NSString *name;
-@property (readonly, retain) NSString *text;
-@property (readonly, retain) NSURL *image;
-@property (readonly, retain) NSURL * _Nullable imageGold;
-@property (readonly, retain) NSString *flavorText;
-@property (readonly, retain) NSURL * _Nullable cropImage;
-@property (readonly, retain) NSArray<NSNumber *> *childIds;
+@property (readonly) NSString *name;
+@property (readonly) NSString *text;
+@property (readonly) NSURL *image;
+@property (readonly) NSURL * _Nullable imageGold;
+@property (readonly) NSString *flavorText;
+@property (readonly) NSURL * _Nullable cropImage;
+@property (readonly) NSArray<NSNumber *> *childIds;
 + (NSArray<HSCard *> *)hsCardsFromJSONData:(NSData *)data error:(NSError **)error;
 @end
 
