02c9cc5b38f146e6584868bbabf64f2251daa312
Jinwoo Kim <kidjinwoo@me.com>
2021-07-28 23:09:34 +0900

    Base of HSCardKeyword

---
+++ StoneNamuCore/Domain/Entity/Card/HSCardKeyword.h
@@ -0,0 +1,52 @@
+//
+//  HSCardKeyword.h
+//  HSCardKeywords
+//
+//  Created by Jinwoo Kim on 7/28/21.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, HSCardKeyword) {
+    HSCardKeywordAdapt,
+    HSCardKeywordBattlecry,
+    HSCardKeywordCharge,
+    HSCardKeywordCombo,
+    HSCardKeywordCorrupt,
+    HSCardKeywordCounter,
+    HSCardKeywordDeathrattle,
+    HSCardKeywordDiscover,
+    HSCardKeywordDivineShield,
+    HSCardKeywordEcho,
+    HSCardKeywordFreeze,
+    HSCardKeywordFrenzy,
+    HSCardKeywordImmune,
+    HSCardKeywordInspire,
+    HSCardKeywordInvoke,
+    HSCardKeywordLackey,
+    HSCardKeywordLifesteal,
+    HSCardKeywordMagnetic,
+    HSCardKeywordMegaWindfury,
+    HSCardKeywordNatureSpellDamage,
+    HSCardKeywordOutcast,
+    HSCardKeywordOverkill,
+    HSCardKeywordOverload,
+    HSCardKeywordPoisonous,
+    HSCardKeywordQuest,
+    HSCardKeywordQuestline,
+    HSCardKeywordReborn,
+    HSCardKeywordRecruit,
+    HSCardKeywordRush,
+    HSCardKeywordSecret,
+    HSCardKeywordSidequest,
+    HSCardKeywordSilence,
+    HSCardKeywordSpareParts,
+    HSCardKeywordSpellDamage,
+    HSCardKeywordSpellburst,
+    HSCardKeywordStartOfGame,
+    HSCardKeywordStealth,
+    HSCardKeywordTaunt,
+    HSCardKeywordTradeable,
+    HSCardKeywordTwinspell,
+    HSCardKeywordWindfury
+};
