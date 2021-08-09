b76f3e8296a4f9e19586fd6a5ae6455eb1d6d985
Jinwoo Kim <kidjinwoo@me.com>
2021-08-10 00:58:59 +0900

    tunned layout

--- StoneNamu/Presentation/CardDetails/CardDetailsChildrenContentCollectionViewCompositionalLayout.m
+++ StoneNamu/Presentation/CardDetails/CardDetailsChildrenContentCollectionViewCompositionalLayout.m
@@ -16,13 +16,18 @@
     NSCollectionLayoutItem *item = [NSCollectionLayoutItem itemWithLayoutSize:itemSize];
     
     NSCollectionLayoutSize *groupSize = [NSCollectionLayoutSize sizeWithWidthDimension:[NSCollectionLayoutDimension fractionalWidthDimension:1.0]
-                                                                       heightDimension:[NSCollectionLayoutDimension fractionalWidthDimension:1.5 * 0.25]];
+                                                                       heightDimension:[NSCollectionLayoutDimension fractionalHeightDimension:1.0]];
     
     NSCollectionLayoutGroup *group = [NSCollectionLayoutGroup horizontalGroupWithLayoutSize:groupSize subitems:@[item]];
     
     NSCollectionLayoutSection *section = [NSCollectionLayoutSection sectionWithGroup:group];
     
-    self = [super initWithSection:section];
+    UICollectionViewCompositionalLayoutConfiguration *configuration = [UICollectionViewCompositionalLayoutConfiguration new];
+    configuration.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    
+    self = [super initWithSection:section configuration:configuration];
+    
+    [configuration release];
     
     return self;
 }
