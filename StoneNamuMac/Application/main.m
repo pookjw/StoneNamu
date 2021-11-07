ce79ba7e255c9da9adda25239483c7e222a4403f
Jinwoo Kim <kidjinwoo@me.com>
2021-11-08 04:29:57 +0900

    removed MainMenu.xib

--- StoneNamuMac/Application/main.m
+++ StoneNamuMac/Application/main.m
@@ -6,7 +6,12 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
 int main(int argc, const char * argv[]) {
+    AppDelegate *delegate = [AppDelegate new];
+    NSApplication.sharedApplication.delegate = delegate;
+    [NSApplication.sharedApplication run];
+    [delegate release];
     return NSApplicationMain(argc, argv);
 }
