93d8a06600241ff996d34d5038d056d6ef1fa20d
Jinwoo Kim <kidjinwoo@me.com>
2022-02-24 01:12:31 +0900

    fixed cell is not reconfigured

--- StoneNamu/Presentation/Decks/Decks/DecksViewModel.m
+++ StoneNamu/Presentation/Decks/Decks/DecksViewModel.m
@@ -328,7 +328,7 @@
                     [snapshot appendItemsWithIdentifiers:@[itemModel] intoSectionWithIdentifier:sectionModel];
                     [itemModel autorelease];
                 } else {
-                    BOOL shouldReconfigure = ((itemModel.isEasterEgg == isEasterEgg) && ([itemModel.classSlug isEqualToString:hsCardClass.slug]) && (itemModel.count == count));
+                    BOOL shouldReconfigure = !((itemModel.isEasterEgg == isEasterEgg) && ([itemModel.classSlug isEqualToString:hsCardClass.slug]) && (itemModel.count == count));
                     itemModel.isEasterEgg = isEasterEgg;
                     itemModel.classSlug = hsCardClass.slug;
                     itemModel.count = count;
