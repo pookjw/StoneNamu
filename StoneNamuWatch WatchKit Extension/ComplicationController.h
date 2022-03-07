972dc7d4ae3a43a54552d4a936cb23d3e855f104
Jinwoo Kim <kidjinwoo@me.com>
2022-03-07 10:18:32 +0900

    added StoneNamuWatch

---
+++ StoneNamuWatch WatchKit Extension/ComplicationController.h
@@ -0,0 +1,12 @@
+//
+//  ComplicationController.h
+//  StoneNamuWatch WatchKit Extension
+//
+//  Created by Jinwoo Kim on 3/7/22.
+//
+
+#import <ClockKit/ClockKit.h>
+
+@interface ComplicationController : NSObject <CLKComplicationDataSource>
+
+@end
---
+++ StoneNamuWatch WatchKit Extension/ExtensionDelegate.h
@@ -0,0 +1,12 @@
+//
+//  ExtensionDelegate.h
+//  StoneNamuWatch WatchKit Extension
+//
+//  Created by Jinwoo Kim on 3/7/22.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
